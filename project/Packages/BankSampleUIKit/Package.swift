198cd524baa46341d7014cde0a19272bb726942b
virgilius-santos <virgilius.santos@picpay.com>
2023-11-12 21:49:16 -0300

    fix config

--- project/Packages/BankSampleUIKit/Package.swift
+++ project/Packages/BankSampleUIKit/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "BankSampleUIKit",
-    platforms: [.iOS(.v12)],
+    platforms: [.iOS(.v16)],
     products: [
         .library(
             name: "BankSampleUIKit",
